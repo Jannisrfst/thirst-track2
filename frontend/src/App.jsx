7155e7275df89091b5fa2dabe8de119e11b5b861
Jannisrfst <101525736+Jannisrfst@users.noreply.github.com>
2025-07-03 09:08:22 +0200

    removed: barcode entries from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,26 @@
-import React from 'react';
-import './styles/style.css';
-import { ThirstTrackHeader } from './components/Header';
-import { BarcodeScanner } from './components/BarcodeEntries';
-import { InventoryList } from './components/InventoryList';
-import { ProductDetails } from './components/ProductDetails';
-import { CsvUploader } from './components/CsvUploader';
-import { BarcodeProvider } from './context/BarcodeContext';
+import React from "react";
+import "./styles/style.css";
+import { ThirstTrackHeader } from "./components/Header";
+import { InventoryList } from "./components/InventoryList";
+import { ProductDetails } from "./components/ProductDetails";
+import { CsvUploader } from "./components/CsvUploader";
+import { BarcodeProvider } from "./context/BarcodeContext";
 
 function App() {
   return (
     <BarcodeProvider>
       <main className="container">
         <div className="mainContent">
-        <ThirstTrackHeader />
-        <div className="panelsContainer">
-          <section className="leftPanel">
-            <BarcodeScanner />
-            <CsvUploader />
-            <InventoryList />
-          </section>
-          <section className="rightPanel">
-            <ProductDetails />
-          </section>
-        </div>
+          <ThirstTrackHeader />
+          <div className="panelsContainer">
+            <section className="leftPanel">
+              <CsvUploader />
+              <InventoryList />
+            </section>
+            <section className="rightPanel">
+              <ProductDetails />
+            </section>
+          </div>
         </div>
       </main>
     </BarcodeProvider>
