1ff4fdb0ff2d540c177893d7fa1c491d1308d7df
Jannisrfst <101525736+Jannisrfst@users.noreply.github.com>
2025-07-03 16:49:16 +0200

    debug: trying old pw and tmp as user

--- classes/PersistanceLayer.py
+++ classes/PersistanceLayer.py
@@ -23,7 +23,7 @@ class PersistanceLayer:
         self._db_path: str = os.getenv("DB_NAME", "thirst-track")
         self._host: str = os.getenv("DB_HOST", "192.168.1.114")
         self._user: str = os.getenv("DB_USER", "tmp")
-        self._password: str = os.getenv("DB_PASSWORD", "postgres")
+        self._password: str = os.getenv("DB_PASSWORD", "2437")
         self._port: str = os.getenv("DB_PORT", "5432")
 # 
     def _getConnection(self) -> psycopg2.extensions.connection:
